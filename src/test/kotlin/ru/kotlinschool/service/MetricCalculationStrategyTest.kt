a07922581fa86be14532a4ba33b1ca6e3d52178c
Tatiana Dolgopolova <tdolgopol@mail.ru>
2023-02-20 23:14:08 +0600

    Валидация

---
+++ src/test/kotlin/ru/kotlinschool/service/MetricCalculationStrategyTest.kt
@@ -0,0 +1,22 @@
+package ru.kotlinschool.service
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import ru.kotlinschool.data.CalculateData
+import ru.kotlinschool.exception.ValidationException
+import java.math.BigDecimal
+
+
+class MetricCalculationStrategyTest {
+
+    private val calculationStrategy = MetricCalculationStrategy()
+
+    @Test
+    fun executeTest() {
+        val calculateData = CalculateData(BigDecimal.valueOf(40), 50.0, 2)
+        assertThrows<ValidationException> {
+            calculationStrategy.execute(calculateData)
+        }
+    }
+
+}
