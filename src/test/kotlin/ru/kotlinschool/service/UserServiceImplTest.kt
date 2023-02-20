b272c0284f9e07b5162dbd161497238fe6621634
Tatiana Dolgopolova <tdolgopol@mail.ru>
2023-02-20 16:48:35 +0600

    тесты

--- src/test/kotlin/ru/kotlinschool/service/UserServiceImplTest.kt
+++ src/test/kotlin/ru/kotlinschool/service/UserServiceImplTest.kt
@@ -1,5 +1,6 @@
 package ru.kotlinschool.service
 
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -24,5 +25,35 @@ class UserServiceImplTest : DataTest() {
     fun getHouses() {
         val houses = userService.getHouses(1L)
         Assertions.assertTrue { houses.size == 1 }
+    }
+
+    @Test
+    fun registerFlat(){
+        userService.registerFlat(1L, 1L,1L, "99", 54.0, 2)
+        verify(exactly = 1) { flatRep.save(any()) }
+    }
+
+    @Test
+    fun getFlats() {
+        val flats = userService.getFlats(1L)
+        Assertions.assertTrue { flats.size == 2 }
+    }
+
+    @Test
+    fun getPublicServices(){
+        val services = userService.getPublicServices(1L)
+        Assertions.assertTrue { services.size == 3 }
+    }
+
+    @Test
+    fun addMetric(){
+        userService.addMetric(1L, 1L,145.0)
+        verify(exactly = 1) { metricRep.save(any()) }
+    }
+
+    @Test
+    fun getBill() {
+        val bill = userService.getBill(1L, 2023, 1)
+        Assertions.assertNotNull {  bill }
     }
 }
