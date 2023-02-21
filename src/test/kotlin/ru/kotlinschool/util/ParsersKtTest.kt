877ea7c9b40803caa5670ca19f5258dd6ff42075
SlumdogProgrammer <bolotin99@inbox.ru>
2023-02-21 23:53:21 +0300

    Test for parsers

--- src/test/kotlin/ru/kotlinschool/util/ParsersKtTest.kt
+++ src/test/kotlin/ru/kotlinschool/util/ParsersKtTest.kt
@@ -20,10 +20,6 @@ class ParsersKtTest {
         assertEquals(FlatRegistrationData("27", 50.5, 2), flatData)
     }
 
-    @Test
-    fun parseRates() {
-    }
-
     @ParameterizedTest
     @ValueSource(strings = ["-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-10", "-11", "-12"])
     fun parseMonthMessageGetNumberTest(value: String) {
