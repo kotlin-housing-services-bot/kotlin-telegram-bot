434e71726e7debb21bc9cdc34a6c41379f162660
SlumdogProgrammer <bolotin99@inbox.ru>
2023-02-21 23:52:14 +0300

    Test for parsers

---
+++ src/test/kotlin/ru/kotlinschool/util/ParsersKtTest.kt
@@ -0,0 +1,46 @@
+package ru.kotlinschool.util
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import ru.kotlinschool.bot.handlers.entities.FlatRegistrationData
+import java.time.Year
+
+class ParsersKtTest {
+
+    @Test
+    fun parseFlatDataTest() {
+        // Given
+        val text = " 27\r\n50.5\r\n2"
+        // When
+        val flatData = parseFlatData(text)
+        // Then
+        assertEquals(FlatRegistrationData("27", 50.5, 2), flatData)
+    }
+
+    @Test
+    fun parseRates() {
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-10", "-11", "-12"])
+    fun parseMonthMessageGetNumberTest(value: String) {
+        // Given
+        // When
+        val meterReadings = parseMonthMessageGetNumber(value)
+        // Then
+        assertEquals(value.replace("-", "").toInt(), meterReadings)
+    }
+
+    @Test
+    fun parseYear() {
+        // Given
+        val year = Year.now().value.toString()
+        // When
+        val yearParsed = parseYear(year)
+        // Then
+        assertEquals(2023, yearParsed)
+    }
+}
